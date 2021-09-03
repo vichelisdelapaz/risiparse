f9e6e3fda512bd068b405aec23937318c204e2ce
vichelis <vichelisdelpaz@prefecturedelpaz.com>
2021-09-03 15:32:31 +0200

    Added selectors for 2sucres and removed redudant ones

--- sites_selectors.py
+++ sites_selectors.py
@@ -4,9 +4,7 @@ from enum import Enum
 
 class Jvc(Enum):
     AUTHOR_SELECTOR = ".bloc-header > span"
-    TOTAL_SELECTOR = ".bloc-liste-num-page > span:last-child"
-    TOTAL_SELECTOR1 = ".bloc-liste-num-page > span:nth-last-child(2)"
-    CURRENT_AUTHOR_SELECTOR = ".bloc-header > span"
+    TOTAL_SELECTOR = ".bloc-liste-num-page > span:nth-last-of-type(2)"
     MESSAGE_SELECTOR = ".conteneur-message"
     RISITAS_TEXT_SELECTOR = "[class='txt-msg text-enrichi-forum']"
     TITLE_SELECTOR = "#bloc-title-forum"
@@ -16,11 +14,18 @@ class Jvc(Enum):
 class Jvarchive(Enum):
     AUTHOR_SELECTOR = "[class='h4 text-truncate min-width-0 mb-0 text-primary'] > a"
     TOTAL_SELECTOR = ".page-item:nth-last-child(2)"
-
-    CURRENT_AUTHOR_SELECTOR = "[class='h4 text-truncate min-width-0 mb-0 text-primary'] > a"
     MESSAGE_SELECTOR = "[class='card-body pb-0 px-3 px-md-4 pt-3']"
     RISITAS_TEXT_SELECTOR = ".conteneur-message"
     TITLE_SELECTOR = "[class='h2 text-white d-inline align-middle mb-0 mr-2']"
+    NOELSHACK_IMG_SELECTOR = "img"
+
+
+class Deuxsucres(Enum):
+    AUTHOR_SELECTOR = ".pseudo"
+    TOTAL_SELECTOR = ".pagination > li:nth-last-of-type(2)"
+    MESSAGE_SELECTOR = "[class='row content-message']"
+    RISITAS_TEXT_SELECTOR = "[class='row content-message']"
+    TITLE_SELECTOR = ".topic-title"
     NOELSHACK_IMG_SELECTOR = "img"
 
 
