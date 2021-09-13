3f7aa048fcbe3b42732b244ed3d6afa303128973
vichelis <vichelisdelpaz@prefecturedelpaz.com>
2021-09-13 17:17:10 +0200

    Updated help, moved parse in put link to utils, format changes

--- risiparse/utils/utils.py
+++ risiparse/utils/utils.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
 
+"""This module just regroup some routines"""
+
+from typing import List, Union
 import unicodedata
 import pathlib
 import re
 import sys
 import argparse
 import logging
+
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
 from risiparse import html_to_pdf, sites_selectors
 
-from typing import List
 
 def write_html(
-        title:str,
+        title: str,
         author: str,
         posts: List,
         output_dir: 'pathlib.Path',
@@ -40,19 +45,21 @@ def write_html(
             <html lang='fr'>
             <head>
             <meta charset='UTF-8'>
-            <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+            <meta name='viewport' \
+            content='width=device-width, initial-scale=1.0'>
             <title>Risitas</title>
             </head>
             <body>"""
         )
         f.write(html)
-        for x in posts:
-            f.write(str(x[0]))
+        for paragraph in posts:
+            f.write(str(paragraph[0]).replace("’", "'"))
         html = (
             """</body>
             </html>"""
         )
         f.write(html)
+        logging.info(f"Wrote {html_path}")
 
 
 def _remove_risitas(title: str) -> str:
@@ -67,8 +74,8 @@ def _replace_whitespaces(title: str) -> str:
 
 def slugify(
         string: str,
-        allow_unicode=False,
-        title:bool = False
+        allow_unicode: bool = False,
+        title: bool = False
 ) -> str:
     if title:
         string = _remove_risitas(string)
@@ -85,8 +92,8 @@ def slugify(
     return re.sub(r'[-\s]+', '-', string).strip('-_')
 
 
-def _strip_www_component(url :str) -> str:
-    domain = re.sub(r"www." , "", url)
+def _strip_www_component(url: str) -> str:
+    domain = re.sub(r"www.", "", url)
     return domain
 
 
@@ -101,19 +108,23 @@ def make_dirs(output_dir: 'pathlib.Path') -> None:
     (output_dir / "risitas-pdf").mkdir(exist_ok=True, parents=True)
 
 
-def get_selectors_and_site(link: str) -> None:
+def get_selectors_and_site(
+        link: str
+) -> tuple[
+    Union[
+        'sites_selectors.Jvc',
+        'sites_selectors.Jvarchive',
+        'sites_selectors.Webarchive'
+    ],
+    str
+]:
     domain = get_domain(link)
-    selectors = ""
-    site = ""
     if domain == "jeuxvideo.com":
         selectors = sites_selectors.Jvc
         site = "jvc"
     elif domain == "jvarchive.com":
         selectors = sites_selectors.Jvarchive
         site = "jvarchive"
-    elif domain == "2sucres.org":
-        selectors = sites_selectors.Deuxsucres
-        site = "2sucres"
     return selectors, site
 
 
@@ -123,20 +134,21 @@ def create_pdfs(output_dir: 'pathlib.Path') -> None:
     if htmls:
         app = html_to_pdf.QtWidgets.QApplication([])
         page = html_to_pdf.PdfPage(output_dir)
-        logging.info(f"Creating pdfs...")
+        logging.info("Creating pdfs...")
         page.convert(htmls)
         sys.exit(app.exec_())
 
 
 def read_links(links_file: pathlib.Path) -> List:
-    with open(links_file) as f:
+    links_file = links_file.expanduser().resolve()
+    with open(links_file, encoding='utf-8') as f:
         page_links = [line.strip() for line in f if line.strip() != '']
     return page_links
 
 
 def get_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    default_links = pathlib.Path().cwd() / "risitas-links"
+    default_links = [pathlib.Path().cwd() / "risitas-links"]
     parser.add_argument(
         "--all-messages",
         action="store_true",
@@ -165,11 +177,14 @@ def get_args() -> argparse.Namespace:
         help="Default : False"
     )
     parser.add_argument(
-        "-l","--links",
+        "-l", "--links",
         action="store",
         nargs='+',
         default=default_links,
-        help="The links file, or links from standard input, Default : current dir/risitas-links"
+        help=(
+            "The links file, or links from standard input, "
+            "Default : current dir/risitas-links"
+        )
     )
     # Take a list of identifiers
     parser.add_argument(
@@ -193,7 +208,8 @@ def get_args() -> argparse.Namespace:
         nargs='+',
         help=(
             "List of authors to be matched, by default the author of "
-            "the first post author is considered as the author throughout the whole risitas "
+            "the first post author is considered as the author "
+            "throughout the whole risitas "
             "Default : Empty"
         ),
         required=False,
@@ -217,7 +233,10 @@ def get_args() -> argparse.Namespace:
         '--download-images',
         help=(
             "Whether to download images locally "
-            "If set, this will change all img[src] link to point to the local images "
+            "If set, this will change all img[src] link to point "
+            "to the local images "
+            "Also this will try to download risitas sticker on webarchive "
+            "if they have been 404ed "
             "Default : False"
         ),
         action="store_true",
@@ -230,7 +249,8 @@ def get_args() -> argparse.Namespace:
         action="store_true",
         default=False,
         help=(
-            "If the name of the author is pogo and the current post author is pogo111, it will be downloaded "
+            "If the name of the author is pogo and the current "
+            "post author is pogo111, it will be downloaded "
             "this disables this feature "
             "Default : False"
         )
@@ -248,3 +268,10 @@ def get_args() -> argparse.Namespace:
     return args
 
 
+def parse_input_links(links):
+    links_file = pathlib.Path(links[0])
+    if len(links) == 1 and links_file.exists():
+        page_links = read_links(links_file)
+    else:
+        page_links = links
+    return page_links
