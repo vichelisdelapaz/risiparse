9f367b762385d38de45c45ec0896fd1f0d228767
vichelis <vichelisdelpaz@prefecturedelpaz.com>
2021-09-13 17:19:29 +0200

    Added docstring and removed colorama and pathlib

--- risiparse/html_to_pdf.py
+++ risiparse/html_to_pdf.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
+"""This module takes care of the html to pdf conversion using QWebEngine"""
+
 import os
-import pathlib
 import logging
 
 from PyQt5.QtCore import QUrl
 from PyQt5.QtGui import QPageLayout, QPageSize
 from PyQt5 import QtWidgets, QtWebEngineWidgets
-from colorama import Fore, Back, Style
+
 
 class PdfPage(QtWebEngineWidgets.QWebEnginePage):
     def __init__(self, output_dir):
@@ -31,11 +32,9 @@ class PdfPage(QtWebEngineWidgets.QWebEnginePage):
         self.loadFinished.connect(self._handleLoadFinished)
         self.pdfPrintingFinished.connect(self._handlePrintingFinished)
 
-
     def convert(self, htmls):
         self._htmls = iter(htmls)
         self._fetchNext()
-
 
     def _fetchNext(self):
         try:
@@ -45,17 +44,12 @@ class PdfPage(QtWebEngineWidgets.QWebEnginePage):
             return False
         return True
 
-
     def _handleLoadFinished(self):
         pdf_file = self.current_file.name[:-5] + ".pdf"
         output_file = str(self.pdf_folder_path) + os.sep + pdf_file
-        output_file_path = pathlib.Path(output_file)
         self.printToPdf(output_file, pageLayout=self.layout)
-        logging.info(f"Creating {Fore.MAGENTA}{output_file}{Style.RESET_ALL}")
-
+        logging.info(f"Creating {output_file}")
 
     def _handlePrintingFinished(self):
         if not self._fetchNext():
             QtWidgets.QApplication.quit()
-
-
