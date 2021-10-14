8f0163e8c6439d646818a99df25057156870d896
vichelis <vichelisdelpaz@prefecturedelpaz.com>
2021-10-14 15:50:04 +0200

    Changed test name

--- tests/e2e/test_create_pdfs.py
+++ tests/e2e/test_create_pdfs.py
@@ -13,7 +13,7 @@ SCRIPT = pathlib.Path(__file__).parent / "risiparse" / "risiparse.py"
         ("https://www.jeuxvideo.com/forums/42-51-67616891-1-0-1-0-risitas-comment-j-ai-couche-avec-une-milf-ce-dimanche.htm")
     ],
 )
-def test_authors(monkeypatch, tmp_path, caplog, test_link):
+def test_create_pdfs(monkeypatch, tmp_path, caplog, test_link):
     tmpdir = tmp_path
     tmpdir.mkdir(exist_ok=True)
     testargs = [
