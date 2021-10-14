54ff0da8007e82a195770fcd9e67ddd3c6a3f32a
vichelis <vichelisdelpaz@prefecturedelpaz.com>
2021-10-14 15:19:42 +0200

    Test resize now takes cares of the case where it is/isn't specified.

--- tests/e2e/test_resize_images.py
+++ tests/e2e/test_resize_images.py
@@ -10,12 +10,13 @@ from bs4 import BeautifulSoup
 SCRIPT = pathlib.Path(__file__).parent / "risiparse" / "risiparse.py"
 
 @pytest.mark.parametrize(
-    "test_link",
+    "test_link,resize",
     [
-        ("https://www.jeuxvideo.com/forums/42-51-66574499-1-0-1-0-risitas-au-bout-du-monde-un-khey-au-japon.htm"),
+        ("https://www.jeuxvideo.com/forums/42-51-66574499-1-0-1-0-risitas-au-bout-du-monde-un-khey-au-japon.htm", True),
+        ("https://www.jeuxvideo.com/forums/42-51-66574499-1-0-1-0-risitas-au-bout-du-monde-un-khey-au-japon.htm", False),
     ],
 )
-def test_resize_images(monkeypatch, tmp_path, caplog, test_link):
+def test_resize_images(monkeypatch, tmp_path, caplog, test_link, resize):
     tmpdir = tmp_path
     tmpdir.mkdir(exist_ok=True)
     testargs = [
@@ -26,9 +27,14 @@ def test_resize_images(monkeypatch, tmp_path, caplog, test_link):
         "--download-images",
         "--no-database",
     ]
+    if not resize:
+        testargs.append("--no-resize-images")
     monkeypatch.setattr(sys, 'argv', testargs)
     main()
-    assert 'full scale' in caplog.text
+    if not resize:
+        assert 'full scale' not in caplog.text
+    else:
+        assert 'full scale' in caplog.text
     output_file = caplog.records[-1].getMessage().split()[1]
     output_file_path = pathlib.Path(output_file)
     with open(output_file_path) as f:
@@ -36,4 +42,7 @@ def test_resize_images(monkeypatch, tmp_path, caplog, test_link):
     imgs = soup.select("img")
     for i in range(2,16):
         img = imgs[-i]
-        assert 'width' and 'height' not in img.attrs
+        if not resize:
+            assert 'width' in img.attrs and 'height' in img.attrs
+        else:
+            assert 'width' not in img.attrs and 'height' not in img.attrs
